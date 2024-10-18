4f168c7267745ae541dacd01742c915e15d69a6c
nidhinPc <nidhinpc66666@gmail.com>
2024-10-18 08:37:09 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:diceroll/controller/home_screen_controller.dart';
+import 'package:diceroll/view/home_screen/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => HomeScreenController(),
+      child:
+    MaterialApp(
+      home: HomeScreen(),
+    ),
+    
+    );
+  }
+}
